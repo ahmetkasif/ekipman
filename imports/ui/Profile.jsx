d4a27bc4154fc36711bf2af8f28f9985f17ef996
Ahmet <ksfahmet@gmail.com>
2019-03-31 18:45:25 +0300

    0.1.3
    Hotfix

--- imports/ui/Profile.jsx
+++ imports/ui/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { Component, constructor } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Accounts } from 'meteor/accounts-base';
 import { Image, Header, Label, Icon, Card, Button } from 'semantic-ui-react';
 
 var gravatar = require('gravatar');
@@ -12,7 +11,6 @@ class Profile extends Component {
     super(props);
     this.state = {};
     this.renderProfile = this.renderProfile.bind(this);
-    this.renderPosts = this.renderPosts.bind(this);
   }
 
   renderAuthorPic(mail){
@@ -20,51 +18,6 @@ class Profile extends Component {
       return(
         <Image src={gravatar.url(mail.address)} />
       );
-    } else {
-      return(
-        <Loading/>
-      );
-    }
-  }
-
-  renderAuthorActions(post){
-    if(post.authorId === Meteor.userId()){
-      return(
-        <Button color='teal' onClick={() => this.props.history.push('/editPost/' + post._id, {id: post._id})}>Düzenle</Button>
-      );
-    }
-  }
-
-  renderPosts(){
-    if(this.props.posts != null){
-      return this.props.posts.map((post) => (
-        <Card key={post._id} className="postFrame">
-          <Card.Content className="postFrameHeader" header={
-            <Header as='h4' image>
-              {post.authorMails ? this.renderAuthorPic(post.authorMails[0]) : null}
-              <Header.Content>
-                <Label color='red' horizontal style={{marginBottom: '0.15em'}}>{post.title}</Label>
-                <Header.Subheader style={{cursor: 'pointer'}} onClick={() => this.props.history.push('/profile/' + post.authorName, {username: post.authorName})}><Label basic horizontal>{post.authorName}</Label></Header.Subheader>
-              </Header.Content>
-            </Header>
-          }/>
-          <Card.Content>
-            {post.text}
-          </Card.Content>
-          <Card.Content extra>
-            <Label horizontal as='a' color='green'>Yeni</Label>
-            <Label horizontal as='a' color='red'>Teknoloji</Label>
-            <Label horizontal as='a' color='teal'>Eğitim</Label>
-          </Card.Content>
-          <Card.Content extra>
-            <div className='ui'>
-              <Button color='olive' onClick={() => this.props.history.push('/postDetails/' + post._id, {id: post._id})}>İncele</Button>
-              {this.renderAuthorActions(post)}
-              <Button color='blue' disabled>Paylaş</Button>
-            </div>
-          </Card.Content>
-        </Card>
-      ));
     } else {
       return(
         <Loading/>
@@ -89,9 +42,6 @@ class Profile extends Component {
             <b>Bio: </b>Placeholder text.
             <b>Some information about author: </b>Placeholder text.
           </Card.Content>
-          <Card.Content description={
-            this.renderPosts()
-          }/>
         </Card>
       );
     } else {
@@ -116,21 +66,8 @@ export default ProfileContainer = withTracker(props => {
   });
 
   var user = Meteor.users.findOne({username: props.match.params.username}, {profile: 1, username: 1, emails: 1});
-  posts = [];
-  if(user != undefined){
-    Tracker.autorun(() => {
-      Meteor.subscribe('userPosts', user._id);
-    });
-
-    posts = Posts.find({}, {transform: function (post) {
-      post.authorName = user.username;
-      post.authorMails = user.emails;
-      return post;
-    }}).fetch();
-  }
 
   return{
-    user,
-    posts
+    user
   };
 })(Profile);
